793b0b5abe00299381fb0222f344ae6388653cd8
leoWolf117 <leo_rodrigueslobo@hotmail.com>
2020-10-18 21:34:37 -0300

    Editando a cor dos links do navbar, inserindo imagem e icones no campo 'sonbre', e colocando mais barras ded skills

--- javascript/script.js
+++ javascript/script.js
@@ -5,5 +5,14 @@ $('.skill-per').each(function(){
     var $this = $(this);
     var per = $this.attr('per');
     $this.css("witdh", per+'%');
+    $({animatedValue: 0}).animate({animatedValue: per},{
+        duration: 1000,
+        step: function(){
+            $this.attr('per', Math.floor(this.animatedValue));
+        },
+        complete: function(){
+               $this.attr('per', Math.floor(this.animatedValue) + '%') 
+        }
+    });
 
-})
+});
