3fa6490f53e42577ddae7821f415f6c9d529251d
leoWolf117 <leo_rodrigueslobo@hotmail.com>
2020-10-27 22:08:59 -0300

    reajustando animação na barra skill e centralizando icones

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,6 @@
-
-
 $('.skill-per').each(function(){
-
     var $this = $(this);
     var per = $this.attr('per');
-    $this.css("witdh", per+'%');
-    $({animatedValue: 0}).animate({animatedValue: per},{
-        duration: 1000,
-        step: function(){
-            $this.attr('per', Math.floor(this.animatedValue));
-        },
-        complete: function(){
-               $this.attr('per', Math.floor(this.animatedValue) + '%') 
-        }
-    });
-
+    $this.css("width",per+'%');
 });
+
